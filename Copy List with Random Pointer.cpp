c5724aa8da4671f07bab80d4a23760ec09a3e219
Bambam Thakur <131291624+Shivam-thakur05@users.noreply.github.com>
2024-05-15 20:52:01 +0530

    Create Copy List with Random Pointer.cpp

---
+++ Copy List with Random Pointer.cpp
@@ -0,0 +1,133 @@
+
+//using map (good solution)
+
+class Solution
+{
+    public:
+    void insertAtTail(Node* &head, Node* &tail, int &data){
+        Node* newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    public:
+    Node *copyList(Node *head)
+    {
+        //create a clone list
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+        Node* temp = head;
+        while(temp!=NULL){
+            insertAtTail(cloneHead,cloneTail,temp->data);
+            temp = temp->next;
+        }
+        //create a map
+        unordered_map<Node*, Node*>oldToNewNode;
+        Node* originalNode = head;
+        Node* cloneNode = cloneHead;
+        
+        while(originalNode!=NULL){
+            oldToNewNode[originalNode] = cloneNode;
+            originalNode = originalNode->next;
+            cloneNode = cloneNode->next;
+        }
+        originalNode = head;
+        cloneNode = cloneHead;
+        while(originalNode!=NULL){
+            cloneNode -> arb = oldToNewNode[originalNode -> arb];
+            originalNode = originalNode->next;
+            cloneNode = cloneNode->next;
+        }
+        return cloneHead;
+    }
+
+};
+
+
+
+
+//without using map (optimal solution)
+
+class Solution {
+    private:
+    void insertAtTail(Node* &head, Node* &tail, int d) {
+        Node* newNode = new Node(d);
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail -> next = newNode;
+            tail = newNode;
+        }
+    }
+     public:
+    Node *copyList(Node *head)
+    {
+        //step 1: Create a Clone List
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+        
+        Node* temp = head;
+        while(temp != NULL) {
+            insertAtTail(cloneHead, cloneTail, temp->data);
+            temp = temp -> next;
+        }
+        
+        // step 2: insert nodes of Clone List in between originalList
+        
+        Node* originalNode = head;
+        Node* cloneNode = cloneHead;
+        
+        while(originalNode != NULL && cloneNode != NULL) {
+            Node* next = originalNode -> next;
+            originalNode -> next = cloneNode;
+            originalNode = next;
+            
+            next = cloneNode -> next;
+            cloneNode -> next = originalNode;
+            cloneNode  = next;
+        }
+        
+        // step 3: Random pointer copy
+        originalNode = head;
+        cloneNode = cloneHead;
+        
+        while(originalNode != NULL && cloneNode != NULL) { 
+            
+            if(originalNode -> arb != NULL) {
+                cloneNode -> arb = originalNode -> arb -> next;
+            }
+            else
+            {
+                cloneNode -> arb  = NULL;
+            }
+            
+            cloneNode = cloneNode -> next;
+            originalNode = originalNode -> next;
+        }
+        
+        //step 4: revert step 2 changes
+        Node* original = head;
+        Node* copy = cloneHead;
+        
+         while (original && copy)
+            {
+                original->next =
+                 original->next? original->next->next : original->next;
+         
+                copy->next = copy->next?copy->next->next:copy->next;
+                original = original->next;
+                copy = copy->next;
+            }
+
+        // step 5 answer return
+        return cloneHead;
+    }
+};
